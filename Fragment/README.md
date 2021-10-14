e12e6e22840bc541dea04ef92ace7d417b7fae9a
selviatunhasanah <selviatunhasanah1@gmail.com>
2021-10-14 08:34:17 +0700

    first commit

---
+++ Fragment/README.md
@@ -0,0 +1,13 @@
+Nama  : Cindy Oktaviani Eka Prasasti
+
+NIM   : E41200289
+
+Prodi : Teknik Informatika
+
+Gol   : A
+
+TUGAS MINGGU 5 ( BUTTOM NAVIGATION )
+
+HASIL PEMBAHASAN :
+
+![Buttom Navigation](https://user-images.githubusercontent.com/80673338/137072933-56c45815-513a-4d47-bb9a-8a5a99e939e8.png)
---
+++ Fragment/app/src/main/java/com/example/bottomnavigationcindy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.bottomnavigationcindy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+
+
+    }
+}
